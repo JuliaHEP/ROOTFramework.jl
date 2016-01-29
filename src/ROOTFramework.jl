c6d3e8f3542e3d17caa29dc33fc1dc7fa041dff7
Oliver Schulz <oschulz@mpp.mpg.de>
2016-01-29 09:00:25 +0100

    Using EasyPkg

--- src/ROOTFramework.jl
+++ src/ROOTFramework.jl
@@ -1,5 +1,13 @@
+# This file is a part of ROOTFramework.jl, licensed under the MIT License (MIT).
+
+# __precompile__()
+
 module ROOTFramework
 
-# package code goes here
+import EasyPkg
+EasyPkg.include_all_sources()
+
+function __init__()
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,4 @@
-using ROOTFramework
-using Base.Test
+# This file is a part of ROOTFramework.jl, licensed under the MIT License (MIT).
 
-# write your own tests here
-@test 1 == 1
+import EasyPkg
+EasyPkg.run_all_tests()
