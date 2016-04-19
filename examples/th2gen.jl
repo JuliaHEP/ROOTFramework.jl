8aace907561ce31e5c88655c59b11e724bf0a6c9
Oliver Schulz <oschulz@mpp.mpg.de>
2016-04-20 01:56:02 +0200

    Added ROOT color palettes

--- examples/th2gen.jl
+++ examples/th2gen.jl
@@ -2,14 +2,16 @@ using Cxx, ROOTFramework, Distributions, PDMats
 import JSON
 
 rootgui()
+set_default_palette(:viridis)
 
-hist = THxx((-20:0.2:20, -20:0.2:20), "hist", "Hist")
+hist = THxx((-10:0.5:10, -10:0.5:10), "hist", "Hist")
 
 dist = MvNormal([0.0, 0.0], ScalMat(2, 5.0))
-@time for i in 1:10000
+@time for i in 1:100000
     push!(hist, (rand(dist)...))
 end
-draw(hist, "col2")
+draw(hist, "lego2")
+# draw(hist, "colz1")
 
 tfile = TFile("out.root", "recreate")
 write(tfile, hist)
